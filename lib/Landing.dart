168a30afe4306770b20a06b6fc1c9543cc8304ed
sreeragm3 <sreeragmcreative@gmail.com>
2024-10-21 21:14:57 +0530

    mobile ui base ready

--- lib/Landing.dart
+++ lib/Landing.dart
@@ -42,21 +42,32 @@ class _LandingState extends State<Landing> {
       name_s = second_w*0.1;
       ico_len = third_w*0.5;
       name_img = 'https://i.imgur.com/ZTqEUAa.png';
-      return  Scaffold(backgroundColor: Colors.orange,);
+      return  Scaffold(body: Column(children: [
+        Container(height: screenHeight*0.1,width:screenWidth,color: Colors.white,),
+        Container(height: screenHeight*0.2,width: screenWidth,decoration: BoxDecoration(image: DecorationImage(image: NetworkImage('https://i.imgur.com/Xhtr9N6.png'),),color: Color(0xFF8184D3),)),
+        Container(height: screenHeight*0.2,width: screenWidth,color:Color(0xFF8184D3) ,child: Padding(padding: EdgeInsets.fromLTRB(0, 0, screenWidth*0.3, 0),child: Image.network('https://i.imgur.com/ZTqEUAa.png'),),),
+        Container(height: screenHeight*0.4,width: screenWidth,color: Colors.purple,child:
+                Row(children: [
+                      SizedBox(width: screenWidth*0.02,) ,
+                      Column(children: [SizedBox(height: screenHeight*0.01,),Container(width: screenWidth*0.45,height: screenHeight*0.25,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color: Colors.black,),),SizedBox(height: screenHeight*0.01,),Container(width: screenWidth*0.45,height: screenHeight*0.12,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color: Colors.deepPurple,),),SizedBox(height: screenHeight*0.01,),],),
+                      SizedBox(width: screenWidth*0.05,),
+                      Column(children:[SizedBox(height: screenHeight*0.01,),Container(width: screenWidth*0.45,height: screenHeight*0.1,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color: Colors.amberAccent,)),SizedBox(height: screenHeight*0.01,),Container(width: screenWidth*0.45,height: screenHeight*0.27,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color: Colors.green,))],),SizedBox(height: screenHeight*0.01,),],),),
+                      SizedBox(width: screenWidth*0.02,),
+        Container(height: screenHeight*0.1,width: screenWidth,color: Colors.lightGreen)],),);
 
 
           }
     else{
-      int x=0;
-      var Names = ['NAMASTE','BONJOUR','NAMASKARAM','WELCOME','VANAKAM'];
-      String hello = '';
-      Timer.periodic(const Duration(seconds: 4), (timer) {
-        setState(() {
-          hello = Names[timer.tick%5];
-          print(hello);
-        });
-
-        });
+      // int x=0;
+      // var Names = ['NAMASTE','BONJOUR','NAMASKARAM','WELCOME','VANAKAM'];
+      // String hello = '';
+      // Timer.periodic(const Duration(seconds: 4), (timer) {
+      //   setState(() {
+      //     hello = Names[timer.tick%5];
+      //     print(hello);
+      //   });
+      //
+      //   });
       port = third_w*0.12;
       port_h = 0;
       port_h2 = third_h*0.0;
@@ -77,11 +88,11 @@ class _LandingState extends State<Landing> {
         ),),Container(child:Column(children: [Container(child:  Padding(
           padding:  EdgeInsets.fromLTRB(0, port_h, third_w*0.015, port_h2),
           child: Text(" PORTFOLIO",style: TextStyle(color: Colors.black,fontSize:port, fontFamily: 'SPARTEN',fontWeight: FontWeight.w900,letterSpacing: 30),),
-        ),height: third_h*0.25,width:screenWidth*40 ,decoration: BoxDecoration(color: Colors.white),),
-          Container(child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [Container(child: Center(child: Text(hello,style: TextStyle(color: Colors.white,fontFamily: 'SPARTEN',fontSize: port*0.6),)),decoration: BoxDecoration(color: Colors.black,borderRadius: BorderRadius.circular(32)),height: third_h*0.4,width: third_w*0.6,),SizedBox(height: third_h*0.05,width: third_w*0.05),Container(child: Column(children: [Padding(
+        ),height: third_h*0.25,width:third_w ,decoration: BoxDecoration(color: Colors.white),),
+          Container(child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [Container(decoration: BoxDecoration(image: DecorationImage(image: NetworkImage('https://i.imgur.com/3QmEQhJ.png'),fit: BoxFit.cover),color: Colors.black,borderRadius: BorderRadius.circular(32)),height: third_h*0.4,width: third_w*0.6,),SizedBox(height: third_h*0.05,width: third_w*0.05),Container(child: Column(children: [Padding(
             padding:  EdgeInsets.fromLTRB(0, third_h*0.005, 0, third_h*0.015),
-            child: Container(height: third_h*0.19,width: third_w*0.3,decoration: BoxDecoration(color: Color(0xFFA9DAD7),borderRadius: BorderRadius.circular(32)),),
-          ),Container(decoration: BoxDecoration(color: Color(0xFF8e6fbd),borderRadius: BorderRadius.circular(32)),height: third_h*0.185,width: third_w*0.3),],),decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(32)),height: third_h*0.4,width: third_w*0.3)],),height: 290,width:screenWidth*40,decoration: BoxDecoration(color: Colors.white),),
+            child: Container(child: Center(child: Text('PROJECTS',style: TextStyle(color: Colors.white,fontFamily: 'SPARTEN',fontSize: third_w*0.05),)),height: third_h*0.19,width: third_w*0.3,decoration: BoxDecoration(color: Color(0xFFA9DAD7),borderRadius: BorderRadius.circular(32)),),
+          ),Container(child: Center(child: Text('ABOUT',style: TextStyle(fontFamily: 'SPARTEN',fontSize: third_w*0.05,color: Colors.white))),decoration: BoxDecoration(color: Color(0xFF8e6fbd),borderRadius: BorderRadius.circular(32)),height: third_h*0.185,width: third_w*0.3),],),decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(32)),height: third_h*0.4,width: third_w*0.3)],),height: 290,width:screenWidth*40,decoration: BoxDecoration(color: Colors.white),),
           Container(child: Padding(
             padding:  EdgeInsets.fromLTRB(0, third_h*0.05, 0, 0),
             child: Row(mainAxisAlignment: MainAxisAlignment.center,children: [Container(height: third_h*0.3,width: third_w*0.25,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color:Color(0xFF595959) ),),SizedBox(width: third_w*0.045,),Container(height: third_h*0.3,width: third_w*0.65,decoration: BoxDecoration(borderRadius: BorderRadius.circular(32),color:Color(0xFFffc367) ),)],),
